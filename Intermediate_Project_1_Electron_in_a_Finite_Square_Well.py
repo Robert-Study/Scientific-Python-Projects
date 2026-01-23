f28b3d4cbe07f8635433fbcb82d1ebc461ed41a9
Robert-Study <72826148+Robert-Study@users.noreply.github.com>
2026-01-23 19:42:39 +0000

    Update Intermediate_Project_1_Electron_in_a_Finite_Square_Well.py

--- Intermediate_Project_1_Electron_in_a_Finite_Square_Well.py
+++ Intermediate_Project_1_Electron_in_a_Finite_Square_Well.py
@@ -49,7 +49,7 @@ from scipy import optimize
 from module_engine.assignment import Assignment1
 
 # Parameters (either use module engine or use some example parameters, illustration only)
-#a, V0 = Assignment1.get_parameters() #Required to be running module engine
+#a, V0 = Assignment1().get_parameters() #Required to be running module engine
 a  = 1.0e-9   #1 nm
 V0 = 10.0     #10 eV
 
